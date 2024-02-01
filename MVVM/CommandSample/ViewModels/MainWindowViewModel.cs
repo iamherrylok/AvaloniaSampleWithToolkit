962c7efbb00176268df8ea421bf6123e10e3331e
iamherrylok <iamherrylok@gmail.com>
2024-02-01 21:37:12 +0800

    添加CommandSample项目

---
+++ MVVM/CommandSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CommandSample.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty] private List<string> _conversationLog;
+
+    [ObservableProperty] private string _robotName;
+
+    [RelayCommand]
+    private void OpenThePodBayDoors()
+    {
+        ConversationLog.Clear();
+        ConversationLog.Add("I'm sorry, Dave, I'm afraid I can't do that.");
+    }
+}
